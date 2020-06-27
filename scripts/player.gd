de1e3d1713ce54c9e22365a6b09ebdf613c90cdc
Ashish <sendashishthakur@gmail.com>
2020-06-27 20:17:09 +0530

    Updated player script
    created code for player animation and othe updateds

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,11 +8,21 @@ var jumpforce = 500
 func  _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -Speed
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("walk")
+		
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = Speed
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idel")
+	if not is_on_floor():
+		$AnimatedSprite.play("jump")
 	
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = -jumpforce
+		
 	velocity.x = lerp(velocity.x,0,0.2)
 	velocity.y = velocity.y + Gravity
 	
